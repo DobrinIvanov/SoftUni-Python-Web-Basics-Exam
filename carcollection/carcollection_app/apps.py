9a50b9760c52e3308815ec3538c1cab74e7e56c8
Dobrin Ivanov <dobsc2@gmail.com>
2022-10-30 09:11:18 +0200

    configure settings.py; urls

---
+++ carcollection/carcollection_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarcollectionAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'carcollection.carcollection_app'
